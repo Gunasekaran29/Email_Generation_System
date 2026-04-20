76d4566089f8ca435a1f90ead8b2eceda144d3b4
santhosh1524 <mgsanthosh59150@gmail.com>
2026-04-20 23:16:07 +0530

    add root endpoint

--- backend/src/main/java/com/mailapp/MailappApplication.java
+++ backend/src/main/java/com/mailapp/MailappApplication.java
@@ -2,10 +2,21 @@ package com.mailapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MailappApplication {
     public static void main(String[] args) {
         SpringApplication.run(MailappApplication.class, args);
+    }
+}
+
+@RestController
+class RootController {
+
+    @GetMapping("/")
+    public String home() {
+        return "RUNNING";
     }
 }
